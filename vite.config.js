fef2da78e90e9d738da5d3bd853f76e1fb27fcca
Inshrah Maryam <inshrahmaryam68@gmail.com>
2025-07-27 21:36:57 +0500

    update routes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react-shopping-cart/', // 👈 ADD THIS LINE
+  // base: mode === 'production' ? '/react-shopping-cart/' : '/',
   plugins: [react()],
 })
