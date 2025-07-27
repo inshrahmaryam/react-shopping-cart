d8ade1f7c908fa0e2ae344e5fdf523d383e71c26
Inshrah Maryam <inshrahmaryam68@gmail.com>
2025-07-27 21:52:41 +0500

    update with HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import HomePage from './assets/pages/HomePage';
 import Home from './assets/pages/Home';
@@ -13,18 +13,16 @@ const BASE_URL='https://fakestoreapi.com/products';
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/LoginSignup" element={<LoginSignup />} />
-   {/* <Route path="/product/:id" element={<ProductDetails />} /> */}
-  <Route element={ <ProtectedRoutes/> } >
-   <Route path="/home" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-      
-          <Route path="/product/:id"  element={  <Details /> }/> 
-  </Route>    
-       
-      </Routes>
+    <Routes>
+  <Route path="/" element={<LoginSignup />} />
+  <Route path="/login" element={<LoginSignup />} />
+  <Route element={<ProtectedRoutes />}>
+    <Route path="/home" element={<Home />} />
+    <Route path="/cart" element={<Cart />} />
+    <Route path="/product/:id" element={<Details />} />
+  </Route>
+</Routes>
+
     </Router>
   );
 }
