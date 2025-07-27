496ec20736eebd5411fc37daf991099c6fb847ab
Inshrah Maryam <inshrahmaryam68@gmail.com>
2025-07-27 21:41:33 +0500

    update with browser router

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: mode === 'production' ? '/react-shopping-cart/' : '/',
+    base: mode === 'production' ? '/react-shopping-cart/' : '/',
   plugins: [react()],
 })
